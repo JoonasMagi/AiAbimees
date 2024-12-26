32dd57d3568ad9ef26a0ee7b8cd68b247addb87c
JoonasMagi <joonas.magi@vikk.ee>
2024-12-26 18:14:47 +0200

    Todo: After signing in, I am taken to /plants and I will see a list of my existing plants
    "Closes #3"

--- server/app.js
+++ server/app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT || 4000; // Võtab porta `.env` failist või kasutab vaikimisi 4000
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
+// Teeninda staatilised failid kaustast 'public'
+app.use(express.static(path.join(__dirname, '../public')));
+
+// API, et saada taimede nimekiri
 app.get('/plants', (req, res) => {
     db.query('SELECT * FROM plants', (err, results) => {
         if (err) {
@@ -25,6 +31,7 @@ app.get('/plants', (req, res) => {
     });
 });
 
+// Käivita server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
